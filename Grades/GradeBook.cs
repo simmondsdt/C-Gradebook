56e45c09764a98b49a253bd6347eac93c32d88a7
Daniel Simmonds <simmondsdt@gmail.com>
2017-05-15 12:14:42 -0600

    make the gradebook show average highest and lowest grades

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -4,16 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Grades 
-{
-	class GradeBook 
-	{
-		public void AddGrade(float grade) 
-		{
+namespace Grades {
+	class GradeBook {
+
+		public GradeBook() {
+			grades = new List<float>();
+		}
+
+		public GradeStatistics ComputeStatistics() {
+			GradeStatistics stats = new GradeStatistics();
+			stats.HighestGrade = 0;
+
+			float sum = 0;
+			foreach(float grade in grades) {
+				stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+				stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+				sum += grade;
+			}
+			stats.AverageGrade = sum / grades.Count;
+
+			return stats;
+		}
+		
+		public void AddGrade(float grade) {
 			grades.Add(grade);
 		}
 
-		List<float> grades = new List<float>();
+		private List<float> grades;
 	}
 }
  
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Grades
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace Grades {
+	class Program {
+		static void Main(string[] args) {
 			GradeBook book = new GradeBook();
 			book.AddGrade(91);
 			book.AddGrade(89.5f);
+			book.AddGrade(75);
+
+			GradeStatistics stats = book.ComputeStatistics();
+			Console.WriteLine("Average Grade: " + stats.AverageGrade);
+			Console.WriteLine("Highest Grade: " + stats.HighestGrade);
+			Console.WriteLine("Loweset Grade: " + stats.LowestGrade);
 		}
 	}
 }
